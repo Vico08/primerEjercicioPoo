488cc38d1e28e5b27d83976a4a3bcd44fea15de2
Victoria Cano <victoriacano@outlook.es>
2024-10-30 19:47:49 -0500

    clase creada con sus propiedades

---
+++ primerEjercicioPoo/Personas.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primerEjercicioPoo
+{
+    internal class Personas
+    {
+     //propiedades de mi clase Personas
+     
+     public string Nombre { get; set; } //forma definir propiedades 
+
+        public int Edad { get; set; }
+
+        //metodo de mi clase Personas
+        // presentarsse
+
+        public void Presentar()
+                {
+                Console.WriteLine($"Hola! mi nombre es {Nombre} y tengo {Edad} años");
+                }
+        public void EsmayorDeEdad()
+        {            
+            if (Edad >= 18)
+            {
+                Console.WriteLine("eres mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("eres menor de edad");
+            }
+        }
+    }
+}
+
---
+++ primerEjercicioPoo/Program.cs
@@ -0,0 +1,7 @@
+﻿private class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
